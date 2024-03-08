eac5b58b069d6bf647ff41c929def51213627c7a
jqvistt <joakim.is.lonnqvist@gmail.com>
2024-03-08 10:34:53 +0200

    Add Scroll prompt and polish first slide

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,21 @@
+const ScrollDownPrompt = document.getElementById("scrollDownPrompt");
+
 document.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("fade-in");
+
+  setTimeout(function () {
+    DisplayScrollDownPrompt();
+  }, 5000);
 });
 
 document.addEventListener("click", function () {
   console.log("clicked");
 });
 
-setTimeout(() => {
-  IntroSequence();
-}, 3000);
+function DisplayScrollDownPrompt() {
+  ScrollDownPrompt.classList.add("fade-in");
+}
+
+window.addEventListener("scroll", function () {
+  ScrollDownPrompt.classList.remove("fade-in");
+});
